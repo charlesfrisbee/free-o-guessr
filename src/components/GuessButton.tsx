edc25d997729fa780f6cccbc2d6aac1a4d8dd3a3
Charles Harris <char5653@uni.sydney.edu.au>
2025-05-23 17:05:55 +0100

    navigate to result page on guess

--- src/components/GuessButton.tsx
+++ src/components/GuessButton.tsx
@@ -1,10 +1,22 @@
 import { useMapStore } from "@/store/map";
+import { useRouter } from "next/navigation";
 
 const GuessButton = () => {
   const currentGuess = useMapStore((s) => s.currentGuess);
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (currentGuess) {
+      // TODO: send guess to server
+
+      router.push("/result");
+    }
+  };
+
   return (
     <button
       disabled={currentGuess ? false : true}
+      onClick={handleClick}
       //   onClick={handleGuessClick}
       className={`z-50 w-full cursor-pointer rounded-full ${
         currentGuess ? "bg-[#6cb928]" : "bg-[rgba(16,16,28,.6)]"
