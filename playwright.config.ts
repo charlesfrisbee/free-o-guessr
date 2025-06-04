f5f4e325fcfda0dc1c319f37fc5075beac80a984
Charles Harris <char5653@uni.sydney.edu.au>
2025-06-04 12:15:20 +0100

    reset web server command to preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     // command: "pnpm run dev",
-    command: "npx wrangler d1 migrations apply free-o-guessr && pnpm run dev",
+    command:
+      "npx wrangler d1 migrations apply free-o-guessr && pnpm opennextjs-cloudflare preview -- --port 3000 --inspector-port 8770",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
