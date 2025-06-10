c44c85ac6a379f25383d6fca89bc2c5f882fed84
Charles Harris <char5653@uni.sydney.edu.au>
2025-06-10 16:08:55 +0100

    replace external close icon with local SVG and add alt text

--- src/components/MobileMapCloseButton.tsx
+++ src/components/MobileMapCloseButton.tsx
@@ -10,7 +10,7 @@ const MobileMapCloseButton = () => {
         }}
         className="z-50 flex h-8 w-8 items-center justify-center rounded-full bg-[rgba(0,0,0,.9)] hover:cursor-pointer"
       >
-        <img src="https://www.geoguessr.com/_next/static/images/close-1facaa8a3bbabb8df3196a38c71bb36a.svg" />
+        <img src="/close-icon.svg" alt="Close map" width="16" height="16" />
       </button>
     </div>
   );
